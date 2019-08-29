1cb59da26efba2557537b6b676b2719996730113
Rafael Sergio <rafa.sergio.02@live.com>
2019-08-28 22:29:18 -0300

    transformado objeto em array para rederizar dinamicamente os recheios

--- src/components/Burger/Burger.jsx
+++ src/components/Burger/Burger.jsx
@@ -3,13 +3,20 @@ import './Burger.css'
 import Ingredients from './Ingredients/Ingredients'
 
 const burger = props => {
+  let transformIngredients = Object.keys(props.ingredients).map(igKey => {
+    return [...Array(props.ingredients[igKey])].map((_, i) => {
+      return <Ingredients key={igKey + i} type={igKey}/>
+    })
+  }).reduce((arr, el) => {
+    return arr.concat(el)
+  }, [])
+  if(!transformIngredients.length){
+    transformIngredients = <p>Por favor comece a colocar ingredientes</p>
+  }
   return (
     <div className="burger">
       <Ingredients type="bread-top"/>
-      <Ingredients type="bacon"/>
-      <Ingredients type="cheese"/>
-      <Ingredients type="meat"/>
-      <Ingredients type="salad"/>
+      {transformIngredients}
       <Ingredients type="bread-bottom"/>
     </div>
   );
