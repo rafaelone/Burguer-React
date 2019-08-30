9d662e4f387c421742d240dad6a59d8f40715dd1
rafael <rafael.sergio@nextpage.com.br>
2019-08-30 08:45:27 -0300

    mostrando o preço final

--- src/containers/BurguerBuilder/BurgerBuilder.jsx
+++ src/containers/BurguerBuilder/BurgerBuilder.jsx
@@ -67,6 +67,7 @@ class BurgerBuilder extends Component{
           ingredientAdded={this.addIngredientHandler}
           ingredientRemoved={this.removeIngredientHandler}
           disabled={disableInfo}
+          price={this.state.totalPrice}
           />
       </Aux>
     )
