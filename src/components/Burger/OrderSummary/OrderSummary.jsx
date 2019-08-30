89f79c7b16c197a23a0c55210fd679960b64bc86
rafael <rafael.sergio@nextpage.com.br>
2019-08-30 10:36:07 -0300

    colocando preço no modal

--- src/components/Burger/OrderSummary/OrderSummary.jsx
+++ src/components/Burger/OrderSummary/OrderSummary.jsx
@@ -14,6 +14,7 @@ const ingredientSummary = Object.keys(props.ingredients)
       <ul>
         {ingredientSummary}
       </ul>
+      <p><strong>Total Price: {props.price.toFixed(2)}</strong> </p>
       <p>Continue to Checkout ?</p>
       <Button btnType="danger" clicked={props.purchaseCancelled}>CANCEL</Button>
       <Button btnType="success" clicked={props.purchaseContinued}>CONTINUE</Button>
