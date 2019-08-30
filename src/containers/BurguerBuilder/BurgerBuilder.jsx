aac762b5f4bc24b523e8ea3a12d3534fdfc9e5b9
Rafael Sergio <rafa.sergio.02@live.com>
2019-08-29 22:12:49 -0300

    criado funcao para adicionar ingredientes e acrescentar o valor

--- src/containers/BurguerBuilder/BurgerBuilder.jsx
+++ src/containers/BurguerBuilder/BurgerBuilder.jsx
@@ -1,24 +1,53 @@
 import React, { Component } from 'react'
 import Aux from '../../hoc/Aux'
 import Burger from '../../components/Burger/Burger'
+import BuildControls from '../../components/Burger/BuildControls/BuildControls'
+
+const INGREDIENT_PRICES = {
+  salad: 0.5,
+  cheese: 0.4,
+  meat: 1.3,
+  bacon: 0.7
+}
+
 class BurgerBuilder extends Component{
   constructor(props){
     super(props)
     this.state = {
       ingredients: {
-        salad: 1,
-        bacon: 1,
-        cheese: 2,
-        meat: 2
-      }
+        salad: 0,
+        bacon: 0,
+        cheese: 0,
+        meat: 0
+      },
+      totalPrice: 4
     }
+  }
+
+  addIngredientHandler = type => {
+    const oldCount  = this.state.ingredients[type]
+    const updatedCounted = oldCount +1;
+    const updateIngredients = {
+      ...this.state.ingredients
+    };
+    updateIngredients[type] = updatedCounted;
+    const priceAddition = INGREDIENT_PRICES[type];
+    const oldPrice = this.state.totalPrice;
+    const newPrice = oldPrice + priceAddition;
+    this.setState({totalPrice: newPrice, ingredients: updateIngredients})
+  }
+
+  removeIngredientHandler = type => {
+
   }
   render(){
     const { ingredients } = this.state;
     return(
       <Aux>
         <Burger ingredients={ingredients}/>
-        <div>Build Controls</div>
+        <BuildControls 
+          ingredientAdded={this.addIngredientHandler}
+          />
       </Aux>
     )
   }
