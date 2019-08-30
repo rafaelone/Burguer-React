0957cf256a4003dd22e52a11e1195fc189662aa3
rafael <rafael.sergio@nextpage.com.br>
2019-08-30 11:10:09 -0300

    criado componentes da toolbar

---
+++ src/components/Navigation/NavigationItems/NavigationItems.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import NavigationItem from './NavigationItem/NavigationItem'
+import './NavigationItems.css'
+
+const navigationItems = props => (
+  <ul className="navigationItems">
+    <NavigationItem link="/" active>Burger Builder</NavigationItem>
+    <NavigationItem link="/">Checkout</NavigationItem>
+  </ul>
+)
+
+export default navigationItems;
