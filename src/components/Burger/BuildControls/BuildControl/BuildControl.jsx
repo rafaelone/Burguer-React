308da49ca4b5ad226b5e3a25301079ab9c8b672a
Rafael Sergio <rafa.sergio.02@live.com>
2019-08-29 22:25:51 -0300

    passando funcao de remover e desabilitar botao por props

--- src/components/Burger/BuildControls/BuildControl/BuildControl.jsx
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import './BuildControl.css'
-const buildControl = ({added, label}) => (
+const buildControl = ({added, label, removed, disabled}) => (
   <div className="buildControl">
     <div className="label">
       {label}
     </div>
-    <button className="less">-</button>
+    <button className="less" onClick={removed} disabled={disabled}>-</button>
     <button className="more" onClick={added}>+</button>
   </div>
 )
