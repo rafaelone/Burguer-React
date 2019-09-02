e095c720104862db5faa66026c7bbbeaacdc4373
rafael <rafael.sergio@nextpage.com.br>
2019-09-02 11:11:19 -0300

    melhorando performance

--- src/components/UI/Modal/Modal.jsx
+++ src/components/UI/Modal/Modal.jsx
@@ -1,18 +1,32 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Aux from '../../../hoc/Aux';
 import Backdrop from '../Backdrop/Backdrop'
 import './Modal.css'
 
-const modal = ({show, children, modalClosed}) => (
-  <Aux>
-    <Backdrop show={show} clicked={modalClosed}/>
-    <div className="modal" style={{
-      transform: show ? 'translateY(0)' : 'translateY(-100vh)',
-      opacity: show ? '1' : 0
-    }}>
-      {children}
-    </div>
-  </Aux>
-);
+class Modal extends Component {
+  
+  constructor(props){
+    super(props)
+  }
 
-export default modal;
+  shouldComponentUpdate(nextProps, nextState){
+    return nextProps.show !== this.props.show;
+  }
+
+  render(){
+    const { show, children, modalClosed } = this.props;
+    return (
+      <Aux>
+        <Backdrop show={show} clicked={modalClosed}/>
+        <div className="modal" style={{
+          transform: show ? 'translateY(0)' : 'translateY(-100vh)',
+          opacity: show ? '1' : 0
+        }}>
+          {children}
+        </div>
+      </Aux>
+    );
+  }
+}
+
+export default Modal;
