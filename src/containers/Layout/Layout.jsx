e42f9e1cc890130417099baa64c7c24ebf175a3b
rafael <rafael.sergio@nextpage.com.br>
2019-09-02 14:40:56 -0300

    add axios,  criado instancia de axios

--- src/containers/Layout/Layout.jsx
+++ src/containers/Layout/Layout.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import Aux from '../../hoc/Aux';
-import Toolbar from '../Navigation/ToolBar/Toolbar'
-import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
+import Toolbar from '../../components/Navigation/ToolBar/Toolbar'
+import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer'
 import './Layout.css'
 
 class Layout extends Component {
