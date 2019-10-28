3203ded1cf327662a829b67c5975128fb81a9e90
Rafael Sergio <rafa.sergio.02@live.com>
2019-10-27 23:00:18 -0300

    add devTools, começando a colocar combine reducers

--- src/index.js
+++ src/index.js
@@ -3,14 +3,15 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
 import { Provider } from 'react-redux';
-import  { createStore } from 'redux';
+import { createStore } from 'redux';
 import reducer from './store/reducer';
 
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-const store = createStore(reducer);
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+const store = createStore(reducer, devTools);
 
 const app = (
   <Provider store={store}>
